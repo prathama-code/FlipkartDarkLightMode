c89c1017f5fd08227cd8e7ef4a46f0a556384a8d
prathama-code <prathamakhartade1999@gmail.com>
2023-04-22 14:56:43 +0530

    first commit

--- fk.js
+++ fk.js
@@ -2,3 +2,13 @@ function darkMode(){
     let mybody = document.body;
     mybody.classList.toggle('mydark');
 }
+
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('main').style.opacity = '0.3'
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity = '1'
+}
