3ec7ecc9aae031ef95f9049f032684d558b799de
prathama-code <prathamakhartade1999@gmail.com>
2023-05-07 12:36:46 +0530

    first commit

--- fk.js
+++ fk.js
@@ -1,7 +1,12 @@
+// DARK-LIGHT MODE
+
 function darkMode(){
     let mybody = document.body;
     mybody.classList.toggle('mydark');
 }
+
+
+// COUPON CODE
 
 function loadCoupon(){
     document.getElementById('coupon').style.visibility = 'visible';
@@ -11,4 +16,22 @@ function loadCoupon(){
 function closeCoupon(){
     document.getElementById('coupon').style.visibility = 'hidden';
     document.getElementById('main').style.opacity = '1'
+}
+
+            
+// geolocation
+
+window.onload = geolocation();
+let x = document.getElementById('out');
+
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }else{
+        x.innerText="Geo Not Supported"
+    }
+}
+
+function showPosition(data){
+    console.log(data)
 }
